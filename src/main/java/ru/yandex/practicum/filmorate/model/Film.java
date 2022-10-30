85dbcc0473a6e917fa4a96e89faf038f21dd33b2
valer0n <voblikovvalera@yandex.ru>
2022-10-30 13:51:03 +0300

    feat: Lombok dependency was added to maven. In classes Film and User were added @Data annotation.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,10 +1,13 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Data;
+
 import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 
+@Data
 public class Film {
     private int id;
     private String name;
