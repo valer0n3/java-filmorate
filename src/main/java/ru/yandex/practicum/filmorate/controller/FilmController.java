40d019af9ca4f1b5240dc205af03c8acfe51cd83
valer0n <voblikovvalera@yandex.ru>
2022-10-30 15:12:18 +0300

    feat: GetMapping was implemented

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,4 +1,27 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/films")
 public class FilmController {
+    private Map<Integer, Film> filmMap = new HashMap();
+
+    @PostMapping
+    public String saveNewFilm(@RequestBody Film film) {
+        filmMap.put(film.getId(), film);
+        return null;
+    }
+
+    @GetMapping
+    public List<Film> getAllFilms() {
+        return new ArrayList<>(filmMap.values());
+    }
 }
