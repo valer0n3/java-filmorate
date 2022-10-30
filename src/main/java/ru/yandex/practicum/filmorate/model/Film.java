22ed74efcb90f99645448072a909b08bcee5f2e6
valer0n <voblikovvalera@yandex.ru>
2022-10-30 13:28:51 +0300

    feat: All fields of Film class werre added.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,4 +1,14 @@
 package ru.yandex.practicum.filmorate.model;
 
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
 public class Film {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDateTime releaseDate;
+    private Duration duration;
 }
