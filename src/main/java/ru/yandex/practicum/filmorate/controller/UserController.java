554b33260960fde307a4cb312ee3f6d175a4df4b
valer0n <voblikovvalera@yandex.ru>
2022-10-31 00:09:42 +0300

    feat: methods for POST, GET and PUt requests were created

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,4 +1,51 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    private Map<Integer, User> userMap = new HashMap();
+    private int counter = 0;
+
+    @PostMapping
+    public User saveNewUser(@RequestBody User user) {
+        int id = incrementId();
+        user.setId(id);
+        userMap.put(id, user);
+        return user;
+    }
+
+    @PutMapping
+    public User updateFilm(@RequestBody User user) {
+        if (checkIfFilmExists(user.getId())) {
+            userMap.put(user.getId(), user);
+        }
+        return user;
+    }
+
+    @GetMapping
+    public List<User> getAllFilms() {
+        return new ArrayList<>(userMap.values());
+    }
+
+    private int incrementId() {
+        return ++counter;
+    }
+
+    private boolean checkIfFilmExists(int id) {
+        for (int userMapKey : userMap.keySet()) {
+            if (id == userMapKey) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
