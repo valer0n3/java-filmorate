d87fbaa88def58e4ff76109d577891ae723afbc8
valer0n <voblikovvalera@yandex.ru>
2022-10-31 00:11:11 +0300

    fix: NonNull annotation was added to Class fields.

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,14 +1,19 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import lombok.NonNull;
 
 import java.time.LocalDate;
 
 @Data
 public class User {
     private int id;
+    @NonNull
     private String email;
+    @NonNull
     private String login;
+    @NonNull
     private String name;
+    @NonNull
     private LocalDate birthday;
 }
