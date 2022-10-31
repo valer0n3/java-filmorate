30e11ac04919f358324c17910dcad93691bad0e1
valer0n3 <voblikovvalera@yandex.ru>
2022-10-31 13:27:21 +0300

    feat: checkReleaseDate() and checkFilmDuration() methods were implemented

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -6,6 +6,8 @@ import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
 import javax.validation.ValidationException;
+import java.time.Duration;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,10 +18,13 @@ import java.util.Map;
 public class FilmController {
     private Map<Integer, Film> filmMap = new HashMap();
     private int counter = 0;
+    private LocalDate earliestReleaseDate = LocalDate.parse("1985-12-28");
 
     @PostMapping
     public Film saveNewFilm(@Valid @RequestBody Film film) {
         checkMaxDescriptionLength(film.getDescription());
+        checkReleaseDate(film.getReleaseDate());
+        checkFilmDuration(film.getDuration());
         int id = incrementId();
         film.setId(id);
         filmMap.put(id, film);
@@ -56,6 +61,22 @@ public class FilmController {
         if (description.length() > 1) {
             throw new ValidationException("Description's length is more than 200 symbols!");
         }
+    }
+
+    private void checkReleaseDate(LocalDate releaseDate) {
+        if (releaseDate.isBefore(earliestReleaseDate)) {
+            throw new ValidationException("Movie's release date can't be earlier than 1985-12-28");
+
+        }
+    }
+
+        private void checkFilmDuration(Duration duration) {
+            if (duration.toMillis() <= 0) {
+                throw new ValidationException("Movie's release date can't be earlier than 1985-12-28");
+
+            }
 
     }
 }
+
+
