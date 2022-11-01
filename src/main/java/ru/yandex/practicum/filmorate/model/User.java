c0a3e56771529a8f03ddeb97aa9ff4b5fb41ae6f
valer0n <voblikovvalera@yandex.ru>
2022-11-01 23:33:52 +0300

    refactor: Annotation @PastOrPresent was added to a field birthday.

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 
 @Data
@@ -19,5 +20,6 @@ public class User {
     private String login;
     private String name;
     @NotNull
+    @PastOrPresent
     private LocalDate birthday;
 }
