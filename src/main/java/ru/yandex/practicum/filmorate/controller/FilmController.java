31b34a6a8104ea56e6bbc64cc44a5dabce5fa592
valer0n3 <voblikovvalera@yandex.ru>
2022-11-14 12:20:23 +0300

    feat: logic was transferred from filmController to FilmStorage

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 
 import javax.validation.Valid;
 import javax.validation.ValidationException;
@@ -21,67 +23,32 @@ import java.util.Map;
 @RestController
 @RequestMapping("/films")
 public class FilmController {
-    private final Map<Integer, Film> filmMap = new HashMap<>();
-    private int counter = 0;
-    private final static LocalDate EARLIEST_RELEASE_DATE = LocalDate.parse("1895-12-28");
-    private final static Logger log = LoggerFactory.getLogger(FilmController.class);
+    private final InMemoryFilmStorage inMemoryFilmStorage;
+
+    @Autowired
+    public FilmController(InMemoryFilmStorage inMemoryFilmStorage) {
+        this.inMemoryFilmStorage = inMemoryFilmStorage;
+
+    }
 
     @PostMapping
     public Film saveNewFilm(@Valid @RequestBody Film film) {
-        checkReleaseDate(film.getReleaseDate());
-        checkFilmDuration(film.getDuration());
-        int id = incrementId();
-        film.setId(id);
-        filmMap.put(id, film);
-        log.info("new Film was successfully added!");
-        return film;
+        return inMemoryFilmStorage.saveNewFilm(film);
     }
 
     @PutMapping
     public Film updateFilm(@Valid @RequestBody Film film) {
-        if (checkIfFilmIdExists(film.getId())) {
-            checkReleaseDate(film.getReleaseDate());
-            checkFilmDuration(film.getDuration());
-            filmMap.put(film.getId(), film);
-            log.info("new Film was successfully updated!");
-            return film;
-        } else {
-            log.info("new Film wasn't updated. Requested ID does not exists!");
-            throw new ValidationException("new Film wasn't updated. Requested ID does not exists!");
-        }
+        return inMemoryFilmStorage.updateFilm(film);
+
     }
 
     @GetMapping
     public List<Film> getAllFilms() {
-        return new ArrayList<>(filmMap.values());
+        return inMemoryFilmStorage.getAllFilms();
+
     }
 
-    private int incrementId() {
-        return ++counter;
-    }
 
-    private boolean checkIfFilmIdExists(int id) {
-        for (int filmMapKey : filmMap.keySet()) {
-            if (id == filmMapKey) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    void checkReleaseDate(LocalDate releaseDate) {
-        if (releaseDate.isBefore(EARLIEST_RELEASE_DATE)) {
-            log.warn("Movie's release date can't be earlier than 1895-12-28");
-            throw new ValidationException("Movie's release date can't be earlier than 1985-12-28");
-        }
-    }
-
-    void checkFilmDuration(int duration) {
-        if (duration <= 0) {
-            log.warn("Movie's duration can not be less than 0");
-            throw new ValidationException("Movie's duration can not be less than 0");
-        }
-    }
 }
 
 
--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -1,4 +1,14 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.List;
+
 public interface FilmStorage {
+    Film saveNewFilm(Film film);
+
+    Film updateFilm(Film film);
+
+    List<Film> getAllFilms();
+
 }
