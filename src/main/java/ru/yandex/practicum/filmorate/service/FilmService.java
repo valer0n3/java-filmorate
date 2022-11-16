6c1b8ce39593c651b4fe70e233e33aba98402a23
valer0n3 <voblikovvalera@yandex.ru>
2022-11-16 11:53:03 +0300

    feat: likeFilm() was implemented

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,7 +1,34 @@
 package ru.yandex.practicum.filmorate.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.ObjectNotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
 @Service
 public class FilmService {
+    private final FilmStorage filmStorage;
+
+    @Autowired
+    public FilmService(FilmStorage filmStorage) {
+        this.filmStorage = filmStorage;
+    }
+
+
+
+    public void likeFilm(long filmId, long userId) {
+        Film likedFilm = filmStorage.getFilmByID(filmId);
+        checkIfFilmObjectIsNull(likedFilm);
+        filmStorage.likeFilm(likedFilm, userId);
+
+
+    }
+
+    private void checkIfFilmObjectIsNull(Film likedFilm) {
+        if (likedFilm == null) {
+            throw new ObjectNotFoundException("Film object is not existed");
+        }
+    }
+
 }
