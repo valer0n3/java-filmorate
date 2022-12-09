1dd93f86a2962fcafadf6b1192805cd09ee1828f
valer0n3 <voblikovvalera@yandex.ru>
2022-12-09 12:22:08 +0300

    fix: add default data into table Rating

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -1,4 +1,36 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
-public class FilmDbStorage {
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.List;
+
+public class FilmDbStorage implements FilmStorage{
+    @Override
+    public Film saveNewFilm(Film film) {
+        return null;
+    }
+
+    @Override
+    public Film updateFilm(Film film) {
+        return null;
+    }
+
+    @Override
+    public List<Film> getAllFilms() {
+        return null;
+    }
+
+    @Override
+    public Film getFilmByID(long id) {
+        return null;
+    }
+
+    @Override
+    public void likeFilm(Film film, User user) {
+    }
+
+    @Override
+    public void deleteFilmsLike(Film film, User user) {
+    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,9 @@ merge  into  GENRE (genre_id, name)
 values (1, 'test1'),
 (2, 'test2'),
 (3, 'test3');
+
+merge  into  RATING ( RATING_TYPE)
+    values ( 'test1'),
+           ( 'test2'),
+           ( 'test3');
+
