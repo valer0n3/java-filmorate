3ff364b1888499c4b9a700fa8fbfc9718413a4e6
valer0n3 <voblikovvalera@yandex.ru>
2022-12-12 00:59:48 +0300

    feat: methods and requests to database was implemented

--- src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreDbStorage.java
@@ -1,17 +1,37 @@
 package ru.yandex.practicum.filmorate.storage.genre;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Genre;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
-public class GenreDbStorage implements GenreStorage{
+@Repository
+public class GenreDbStorage implements GenreStorage {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public GenreDbStorage(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public List<Genre> getAllGenres() {
-        return null;
+        String sqlQueryAllGenreSelect = "select * from GENRE";
+        return jdbcTemplate.query(sqlQueryAllGenreSelect, (rs, rowNum) -> mapRowToListOfGenres(rs));
+    }
+
+    private Genre mapRowToListOfGenres(ResultSet rs) throws SQLException {
+        return new Genre(rs.getLong("GENRE_ID"),
+                rs.getString("NAME"));
     }
 
     @Override
     public Genre getGenreById(long id) {
-        return null;
+        String sqlQueryGenreSelect = "select * from GENRE where GENRE_ID = ?";
+        return jdbcTemplate.queryForObject(sqlQueryGenreSelect, (rs, rowNum) -> mapRowToListOfGenres(rs), id);
     }
 }
