469c35cc1b8aeaf6e3fa70415c486a8f54a9faf5
valer0n3 <voblikovvalera@yandex.ru>
2022-12-12 00:42:56 +0300

    feat: GenreService was implemented

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Genre;
+
+import java.util.List;
+
+@Service
+public class GenreService {
+
+    public List<Genre> getAllGenres () {
+        return null;
+    }
+
+    public Genre getGenreById (long id) {
+        return null;
+    }
+}
