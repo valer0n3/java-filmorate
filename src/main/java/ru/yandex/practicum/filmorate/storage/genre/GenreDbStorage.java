6e324448b2e10608e3683bfb44144aecbb5dd0f0
valer0n3 <voblikovvalera@yandex.ru>
2022-12-12 00:49:01 +0300

    feat: GenreDbStorage  was implemented

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreDbStorage.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.storage.genre;
+
+import ru.yandex.practicum.filmorate.model.Genre;
+
+import java.util.List;
+
+public class GenreDbStorage implements GenreStorage{
+    @Override
+    public List<Genre> getAllGenres() {
+        return null;
+    }
+
+    @Override
+    public Genre getGenreById(long id) {
+        return null;
+    }
+}
