ce49134d82d92f14edf7bca2156d11d2e691d6ca
valer0n3 <voblikovvalera@yandex.ru>
2022-12-14 12:38:47 +0300

    refactor: validation tests were returned to a project.

---
+++ src/test/java/ru/yandex/practicum/filmorate/service/FilmValidationServiceTest.java
@@ -0,0 +1,38 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
+
+import javax.validation.ValidationException;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class FilmValidationServiceTest {
+    private FilmValidationService filmValidationService;
+
+    @BeforeEach
+    void setUp() {
+        filmValidationService = new FilmValidationService(new InMemoryFilmStorage(), new InMemoryUserStorage());
+    }
+
+    @Test
+    public void shouldThrowValidationExceptionWhenReleaseDateIsEarlierThan1895_12_28() {
+        ValidationException exception = assertThrows(ValidationException.class,
+                () -> filmValidationService.checkReleaseDate(LocalDate.of(1895, 12, 27)));
+        assertEquals("Movie's release date can't be earlier than 1985-12-28",
+                exception.getMessage(),
+                "Exception ValidationException is not correctly thrown when date is earlier than 1985-12-28");
+    }
+
+    @Test
+    public void shouldThrowValidationExceptionWhenFilmDurationIsLessThan0() {
+        ValidationException exception = assertThrows(ValidationException.class,
+                () -> filmValidationService.checkFilmDuration(0));
+        assertEquals("Movie's duration can not be less than 0", exception.getMessage(),
+                "Exception ValidationException is not correctly thrown when movies duration = 0");
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/service/UserValidationServiceTest.java
@@ -0,0 +1,40 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
+
+import javax.validation.ValidationException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class UserValidationServiceTest {
+    private UserValidationService userValidationService;
+
+    @BeforeEach
+    void setUp() {
+        userValidationService = new UserValidationService(new InMemoryUserStorage());
+    }
+
+    @Test
+    public void ShouldThrowValidationExceptionWhenLoginHasSpaces() {
+        ValidationException exception = assertThrows(ValidationException.class,
+                () -> userValidationService.checkLogin("tes t"));
+        assertEquals("Login can't have \\s symbols", exception.getMessage(),
+                "Exception ValidationException is not correctly thrown when login has spaces");
+    }
+
+    @Test
+    public void ShouldReturnTrueWhenNameIsNullOrEmpty() {
+        User user = new User();
+        user.setName(null);
+        assertTrue(userValidationService.checkIfNameIsEmpty(user),
+                "Return incorrect False when user's name is null");
+        user.setName(" ");
+        assertTrue(userValidationService.checkIfNameIsEmpty(user),
+                "Return incorrect False when user's name isBlanked");
+    }
+}
